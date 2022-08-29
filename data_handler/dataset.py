042ca24f8da71b10c9f6292eac0dda01aadcd0f1
Sahar <sahar@MacBook-Pro.local>
2022-08-29 16:50:02 +0430

    add functions to save tokenize data to prevent redundancy

--- data_handler/dataset.py
+++ data_handler/dataset.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
+from utils.save_data import save_tokenized_datasets
 import torch
 
 
@@ -11,13 +12,19 @@ def create_train_dev_test_datasets(data, tokenizer, max_len):
 
     print('Tokenizing train data')
     x_train = tokenizer(x_train, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(x_train, 'input_train.pkl')
     y_train = tokenizer(y_train, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(y_train, 'output_train.pkl')
     print('Tokenizing dev data')
     x_dev = tokenizer(x_dev, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(x_dev, 'input_dev.pkl')
     y_dev = tokenizer(y_dev, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(y_dev, 'output_dev.pkl')
     print('Tokenizing test data')
     x_test = tokenizer(x_test, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(x_test, 'input_test.pkl')
     y_test = tokenizer(y_test, padding=True, truncation=True, max_length=max_len)
+    save_tokenized_datasets(y_test, 'output_test.pkl')
 
     train_dataset = WikiDataset(x_train, y_train)
     dev_dataset = WikiDataset(x_dev, y_dev)
--- utils/save_data.py
+++ utils/save_data.py
@@ -1,5 +1,14 @@
+import pickle
+
+
 def save_predictions(input, label, prediction):
 
     with open('preds.csv', 'w') as f:
         for i, l, p in zip(input, label, prediction):
             f.write(f'{i}||{l}||{p}\n')
+
+
+def save_tokenized_datasets(dataset, name):
+
+    with open(name, 'wb') as f:
+        pickle.dump(dataset, f)
