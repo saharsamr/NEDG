537d1993531e3bfcc9936decccafbe17e8a64449
saharsamr <sahar.rajabi76@gmail.com>
2022-09-12 00:41:33 +0430

    use max-length for output

--- data_handler/dataset.py
+++ data_handler/dataset.py
@@ -18,16 +18,16 @@ class WikiDataset(Dataset):
     def __getitem__(self, idx):
 
         input_encodings = self.tokenizer(self.inputs[idx], padding='longest', truncation=True, max_length=self.max_len)
-        output_encodings = self.tokenizer(self.labels[idx], padding='longest', truncation=True, max_length=self.max_len)
+        output_encodings = self.tokenizer(self.labels[idx], padding='max_length', truncation=True, max_length=15)
 
-        print('>>>>>>>>>>>>>>>', len(input_encodings), input_encodings)
-        print('>>>>>>>>>>>>>>>', len(output_encodings), output_encodings)
+        print('>>>>>>>>>>>>>>>', input_encodings)
+        print('>>>>>>>>>>>>>>>', output_encodings)
 
         item = {
             'input_ids': torch.tensor(input_encodings['input_ids']),
             'attention_mask': torch.tensor(input_encodings['attention_mask'])
         }
-        
+
         if self.labels:
             item['labels'] = torch.tensor(output_encodings['input_ids'])
 
