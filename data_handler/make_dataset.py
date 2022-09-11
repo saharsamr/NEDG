f60bd1f7bb41ff9f52303311cc92d59b5d65e40d
Sahar <sahar@MacBook-Pro.local>
2022-09-11 11:25:03 +0430

    add some functions and changes due to the deviation in source of data

--- data_handler/make_dataset.py
+++ data_handler/make_dataset.py
@@ -1,9 +1,15 @@
-from pymongo import MongoClient
-import datasets
+import pickle
+
+# from pymongo import MongoClient
+# import datasets
 from tqdm import tqdm
+from collections import defaultdict
 import logging
 
-from utils.arrays import is_array_of_empty_strings
+import json
+import csv
+
+from utils.data_structures import is_array_of_empty_strings
 
 
 def import_to_mongo():
@@ -15,18 +21,38 @@ def import_to_mongo():
 
     logging.info("Importing data to MongoDB")
     data = datasets.load_dataset("kilt_wikipedia", streaming=True, split='full')
+    categories = defaultdict(int)
+    counter = 0
     for d in tqdm(data):
+
+        if is_array_of_empty_strings(d['anchors']['wikipedia_id']):
+            continue
 
         new_anchor = {'paragraph_id': d['anchors']['paragraph_id'], 'start': d['anchors']['start'],
                       'end': d['anchors']['end'], 'text': d['anchors']['text'],
                       'wikipedia_title': d['anchors']['wikipedia_title'], 'wikipedia_id': d['anchors']['wikipedia_id']}
-        new_record = {'wikipedia_id': d['wikipedia_id'], 'wikipedia_title': d['wikipedia_title'], 'text': d['text'],
-                      'anchors': new_anchor}
+
+        new_record = {
+            'wikipedia_id': d['wikipedia_id'], 'wikipedia_title': d['wikipedia_title'], 'text': d['text'],
+            'categories': d['categories'], 'wikidata_info': d['wikidata_info'], 'anchors': new_anchor
+        }
+
+        cats = d['categories'].split(',')
+        for cat in cats:
+            categories[cat] += 1
+
+        counter += 1
+        if counter % 1000:
+            with open('categories.pkl', 'wb') as f:
+                pickle.dump(categories, f)
 
         collection.insert_one(new_record)
 
     logging.info("Creating indices")
     collection.create_index([('wikipedia_id', 1)])
+
+    with open('total_categories.pkl', 'wb') as f:
+        pickle.dump(categories, f)
 
 
 def create_dataset():
@@ -65,3 +91,17 @@ def create_dataset():
                     f.write(f'{text}\1{context}\1{description}\n')
 
         logging.error("Failed to find {} documents".format(failed_counts))
+
+
+def make_jsonl_to_csv(file_path):
+
+    with open(file_path, 'r') as f:
+        with open(f'{file_path[:-5]}.csv', 'w') as g:
+            writer = csv.writer(g, delimiter='\1')
+            for line in f:
+                data = json.loads(line)
+                key, value = list(data.keys())[0], list(data.values())[0]
+                for context in value['contexts']:
+                    title, context, description = \
+                        value['label'].replace('\1', ''), context.replace('\1', ''), value['description'].replace('\1', '')
+                    writer.writerow([title, context, description])
