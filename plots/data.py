0efddd2f4b1ce0a7482d671ff61702fbddd0b2ec
saharsamr <sahar.rajabi76@gmail.com>
2023-01-21 18:01:27 +0330

    add plotting categories barplot function

--- plots/data.py
+++ plots/data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from collections import defaultdict
+import json
 
 
 def plot_features_len_bar_plots(data_path, feature_name, num_bins=30, hard_limit=None, bw_method=None):
@@ -26,3 +28,21 @@ def plot_features_len_bar_plots(data_path, feature_name, num_bins=30, hard_limit
     plt.show()
 
 
+def plot_categories_bar_plots(data_path):
+
+    categories = defaultdict(int)
+    with open(data_path, 'r') as f:
+        for line in f:
+            data = list(json.loads(line).values())[0]
+            for k, v in data['types'].items():
+                categories[v['label']] += 1
+
+    plt.figure(figsize=(8, 6))
+    categories = {k: v for k, v in sorted(categories.items(), key=lambda item: item[1], reverse=True)[:20]}
+    sns.barplot(x=list(categories.keys()), y=list(categories.values()), color='mediumseagreen')
+    plt.xticks(rotation=90, fontsize=5)
+    plt.yticks(fontsize=5)
+    plt.xlabel('Category')
+    plt.ylabel('Num of Entities')
+    plt.gcf().subplots_adjust(bottom=0.3)
+    plt.show()
