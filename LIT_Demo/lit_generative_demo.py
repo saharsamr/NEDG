58dcdf911c54ca53c37542e2a4cd63b276870315
saharsamr <sahar.rajabi76@gmail.com>
2023-05-01 16:33:20 +0330

    add first encoder/decoder attentions

--- LIT_Demo/lit_generative_demo.py
+++ LIT_Demo/lit_generative_demo.py
@@ -51,6 +51,8 @@ class BartModel(lit_model.Model):
         )
         self.model = BartForConditionalGeneration.from_pretrained(model_path)
 
+        self.enc_layers, self.dec_layers = None, None
+
     def predict_minibatch(self, inputs):
 
         encoded_input = self.tokenizer.batch_encode_plus(
@@ -74,7 +76,7 @@ class BartModel(lit_model.Model):
                 encoded_input[tensor] = encoded_input[tensor].cuda()
 
         with torch.no_grad():  # remove this if you need gradients.
-            out = self.model(**encoded_input)
+            out = self.model(**encoded_input, output_attentions=True)
             ids = self.model.generate(encoded_input['input_ids'])
 
         batched_outputs = {
@@ -83,7 +85,8 @@ class BartModel(lit_model.Model):
             "input_ntok": torch.sum(encoded_input["attention_mask"], dim=1),
             "target_ids": encoded_output["input_ids"],
             "target_ntok": torch.sum(encoded_output["attention_mask"], dim=1),
-            # "cls_emb": out.hidden_states[-1][:, 0],  # last layer, first token
+            "decoder_layer_1_attention": out.decoder_attentions[0],
+            "encoder_layer_1_attention": out.encoder_attentions[0]
         }
 
         detached_outputs = {k: v.cpu().numpy() if type(v) != list else torch.tensor(v).cpu().numpy() for k, v in batched_outputs.items()}
@@ -109,12 +112,15 @@ class BartModel(lit_model.Model):
 
     def output_spec(self) -> lit_types.Spec:
 
-        return {
+        spec = {
             "input_tokens": lit_types.Tokens(),
             "target_tokens": lit_types.Tokens(),
             "output_text": lit_types.GeneratedText(parent="description"),
-            # "cls_emb": lit_types.Embeddings()
+            "encoder_layer_1_attention": lit_types.AttentionHeads(align_in="input_tokens", align_out="input_tokens"),
+            "decoder_layer_1_attention": lit_types.AttentionHeads(align_in="target_tokens", align_out="target_tokens")
         }
+
+        return spec
 
 
 def get_wsgi_app() -> Optional[dev_server.LitServerType]:
@@ -122,13 +128,13 @@ def get_wsgi_app() -> Optional[dev_server.LitServerType]:
     FLAGS.set_default("server_type", "default")
     FLAGS.set_default("host", "0.0.0.0")
     FLAGS.set_default("demo_mode", True)
-    # Parse flags without calling app.run(main), to avoid conflict with
-    # gunicorn command line flags.
+
     unused = flags.FLAGS(sys.argv, known_only=True)
     if unused:
         logging.info(
             "quickstart_sst_demo:get_wsgi_app() called with unused "
             "args: %s", unused)
+
     return main([])
 
 
@@ -137,7 +143,6 @@ def main(argv: Sequence[str]) -> Optional[dev_server.LitServerType]:
     datasets = {'wiki_dataset': WikiDataset('../data/HumanConcatenated/test_human_masked_ne_with_context.csv')}
     models = {"bart": BartModel('../results/ConcatedCME', 'facebook/bart-large-cnn')}
 
-    # Start the LIT server. See server_flags.py for server options.
     lit_demo = dev_server.Server(models, datasets, **server_flags.get_flags())
     return lit_demo.serve()
 
