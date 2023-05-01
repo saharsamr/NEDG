c06c42ccca48f678177e548b9a142b05fb962566
kghezelbash <kghezelbash12@gmail.com>
2023-05-01 14:16:58 +0330

    adding anchors to the mongo db

--- data_handler/add_anchors_to_mongo.py
+++ data_handler/add_anchors_to_mongo.py
@@ -3,7 +3,7 @@
 import pymongo
 import re
 import urllib.parse
-
+from collections import defaultdict
 # Establishing a connection with MongoDB
 client = pymongo.MongoClient('mongodb://localhost:27017/', username = 'user', password = 'pass')
 
@@ -13,16 +13,22 @@ db = client['wikipedia']
 # Creating a collection
 collection = db['dump']
 
-link_list = []
+dictionary = defaultdict(list)
 # open each json
 for document in collection.find():
-    # extract the context
-    context = document["context"]
+    # extract the text
+    context = document["text"]
+    paragraphs = context.split('\n')
     # find anchors and add them to list
-    links = re.findall(r'<a href=(.*?)&gl', context)
+    for i, par in enumerate(paragraphs):
+        links = re.findall(r'<a href=(.*?)&gl', par)
+        for link in links:
+            decoded_link = urllib.parse.unquote(link)
+            dictionary[decoded_link].append(i)
     # link_list.extend(links)
-    link_list = list(set(links))  # remove duplicates
+    # dictionary = list(set(links))  # remove duplicates
+
     # decode the links
-    decoded_anchors = [urllib.parse.unquote(link) for link in link_list]
+    # decoded_anchors = [urllib.parse.unquote(link) for link in dictionary]
     # add the list to the mongo json
-    collection.update_one({"_id": document["_id"]}, {"$set": {"anchors": decoded_anchors}})
+    collection.update_one({"_id": document["_id"]}, {"$set": {"anchors": dictionary}})
