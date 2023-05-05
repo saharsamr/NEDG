41102400ac8c082e31b16db108f80be0c6bc4159
saharsamr <sahar.rajabi76@gmail.com>
2023-05-05 12:07:27 +0330

    add encoder embeddings

--- LIT/lit_models.py
+++ LIT/lit_models.py
@@ -6,6 +6,13 @@ import torch
 from transformers import BartTokenizerFast, BartForConditionalGeneration
 
 
+def masked_token_mean(vectors, masks):
+
+    denom = torch.sum(masks, -1, keepdim=True)
+    feat = torch.sum(vectors * masks.unsqueeze(-1), dim=1) / denom
+    return feat
+
+
 class BartModel(lit_model.Model):
 
     def __init__(self, model_path, model_name):
@@ -49,10 +56,13 @@ class BartModel(lit_model.Model):
             "target_ids": encoded_output["input_ids"],
             "target_ntok": torch.sum(encoded_output["attention_mask"], dim=1),
             "decoder_layer_1_attention": out.decoder_attentions[0],
-            "encoder_layer_1_attention": out.encoder_attentions[0]
+            "encoder_layer_1_attention": out.encoder_attentions[0],
+            "encoder_final_embedding": masked_token_mean(
+                out.encoder_last_hidden_state, encoded_input["attention_mask"])
         }
 
-        detached_outputs = {k: v.cpu().numpy() if type(v) != list else torch.tensor(v).cpu().numpy() for k, v in batched_outputs.items()}
+        detached_outputs = {k: v.cpu().numpy() if type(v) != list else torch.tensor(v).cpu().numpy() for k, v in
+                            batched_outputs.items()}
 
         detached_outputs["output_text"] = self.tokenizer.batch_decode(ids, skip_special_tokens=True)
 
@@ -81,7 +91,8 @@ class BartModel(lit_model.Model):
             "target_tokens": lit_types.Tokens(),
             "output_text": lit_types.GeneratedText(parent="description"),
             "encoder_layer_1_attention": lit_types.AttentionHeads(align_in="input_tokens", align_out="input_tokens"),
-            "decoder_layer_1_attention": lit_types.AttentionHeads(align_in="target_tokens", align_out="target_tokens")
+            "decoder_layer_1_attention": lit_types.AttentionHeads(align_in="target_tokens", align_out="target_tokens"),
+            "encoder_final_embedding": lit_types.Embeddings()
         }
 
         return spec
