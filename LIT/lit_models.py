251c53a6cfca33733729de0637666b3ab504b1d0
saharsamr <sahar.rajabi76@gmail.com>
2023-05-06 21:52:07 +0330

    add other gradients

--- LIT/lit_models.py
+++ LIT/lit_models.py
@@ -43,10 +43,14 @@ class BartModel(lit_model.Model):
         if torch.cuda.is_available():
             self.model.cuda()
             for tensor in encoded_input:
+                try:
+                    encoded_input[tensor].requires_grad = True
+                except:
+                    pass
                 encoded_input[tensor] = encoded_input[tensor].cuda()
 
-        with torch.no_grad():  # remove this if you need gradients.
-            out = self.model(**encoded_input, output_attentions=True)
+        with torch.torch.set_grad_enabled(True):  # remove this if you need gradients.
+            out = self.model(**encoded_input, output_attentions=True, output_hidden_states=True)
             ids = self.model.generate(encoded_input['input_ids'])
 
         batched_outputs = {
@@ -58,10 +62,19 @@ class BartModel(lit_model.Model):
             "decoder_layer_1_attention": out.decoder_attentions[0],
             "encoder_layer_1_attention": out.encoder_attentions[0],
             "encoder_final_embedding": masked_token_mean(
-                out.encoder_last_hidden_state, encoded_input["attention_mask"])
+                out.encoder_last_hidden_state, encoded_input["attention_mask"]),
+            'token_embeddings': out.encoder_last_hidden_state
         }
 
-        detached_outputs = {k: v.cpu().numpy() if type(v) != list else torch.tensor(v).cpu().numpy() for k, v in
+        # <torch.float32>[batch_size, num_tokens, emb_dim]
+        scalar_pred_for_gradients = torch.max(
+            batched_outputs["probas"], dim=1, keepdim=False, out=None)[0]
+        batched_outputs["input_emb_grad"] = torch.autograd.grad(
+            scalar_pred_for_gradients,
+            out.decoder_hidden_states[0],
+            grad_outputs=torch.ones_like(scalar_pred_for_gradients))[0]
+
+        detached_outputs = {k: v.detach().cpu().numpy() if type(v) != list else torch.tensor(v).cpu().numpy() for k, v in
                             batched_outputs.items()}
 
         detached_outputs["output_text"] = self.tokenizer.batch_decode(ids, skip_special_tokens=True)
@@ -73,6 +86,8 @@ class BartModel(lit_model.Model):
             output_ntok = output.pop("target_ntok")
             output["target_tokens"] = self.tokenizer.convert_ids_to_tokens(
                 output.pop("target_ids")[1:output_ntok - 1])
+            output["token_grad_sentence"] = output["input_emb_grad"][1:input_ntok - 1]
+            output['input_token_embedding'] = output['token_embeddings'][1:input_ntok - 1]
             yield output
 
     def input_spec(self) -> lit_types.Spec:
@@ -88,8 +103,9 @@ class BartModel(lit_model.Model):
         spec = {
             "target_text": lit_types.TextSegment(),
             "input_tokens": lit_types.Tokens(),
-            "input_gradients": lit_types.TokenGradients(align="input_tokens"),
             "target_tokens": lit_types.Tokens(),
+            'input_token_embedding': lit_types.TokenEmbeddings(align='input_tokens'),
+            "token_grad_sentence": lit_types.TokenGradients(align="input_tokens", grad_for='input_token_embedding'),
             "output_text": lit_types.GeneratedText(parent="description"),
             "encoder_layer_1_attention": lit_types.AttentionHeads(align_in="input_tokens", align_out="input_tokens"),
             "decoder_layer_1_attention": lit_types.AttentionHeads(align_in="target_tokens", align_out="target_tokens"),
