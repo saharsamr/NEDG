3c0273a0c2343c8598114f6d0ea7f43c8c43a7e4
kghezelbash <kghezelbash12@gmail.com>
2023-05-13 15:36:07 +0330

    adding a new branch

--- models/BART.py
+++ models/BART.py
@@ -7,65 +7,88 @@ from transformers import Trainer
 from torch.utils.data import DataLoader
 import torch
 from config import MODEL_GENERATION_NAME, ADDITIONAL_SPECIAL_TOKENS, \
-    MODEL_GENERATION_PATH, OUTPUT_GENERATION_MAX_LENGTH, LEARNING_RATE, INPUT_GENERATION_MAX_LENGTH, \
-    OUTPUT_GENERATION_MIN_LENGTH, TEST_GENERATION_BATCH_SIZE
+    MODEL_GENERATION_PATH, MODEL_GENERATION_PATH_2, OUTPUT_GENERATION_MAX_LENGTH, LEARNING_RATE, INPUT_GENERATION_MAX_LENGTH, \
+    OUTPUT_GENERATION_MIN_LENGTH, TEST_GENERATION_BATCH_SIZE, MODEL_GENERATION_NAME_2
 from tqdm import tqdm
 
 
 class BART:
 
     def __init__(
-      self, trainer_args,
-      train_x, train_y,
-      test_x, test_y,
-      valid_x, valid_y,
-      model_name=MODEL_GENERATION_NAME,
-      load=False,
+            self, trainer_args,
+            train_x, train_y,
+            test_x, test_y,
+            valid_x, valid_y,
+            model_name=MODEL_GENERATION_NAME,
+            load=False,
+            model_name_2=MODEL_GENERATION_NAME_2,
+            load_2=False,
     ):
 
-        self.model_name = model_name
+        self.model_name_1 = model_name
+        self.model_name_2 = model_name_2
         self.tokenizer = BartTokenizerFast.from_pretrained(
-            self.model_name, model_max_length=INPUT_GENERATION_MAX_LENGTH, padding=True, truncation=True,
+            self.model_name_1, model_max_length=INPUT_GENERATION_MAX_LENGTH, padding=True, truncation=True,
         )
         self.tokenizer.add_special_tokens({'additional_special_tokens': ADDITIONAL_SPECIAL_TOKENS})
         if load:
-            self.model = BartForConditionalGeneration.from_pretrained(MODEL_GENERATION_PATH)
+            self.model_1 = BartForConditionalGeneration.from_pretrained(MODEL_GENERATION_PATH)
         else:
-            self.model = BartForConditionalGeneration.from_pretrained(self.model_name)
-        self.model.resize_token_embeddings(len(self.tokenizer))
+            self.model_1 = BartForConditionalGeneration.from_pretrained(self.model_name_1)
+        self.model_1.resize_token_embeddings(len(self.tokenizer))
+
+        if load_2:  # initialize second model
+            self.model_2 = BartForConditionalGeneration.from_pretrained(MODEL_GENERATION_PATH_2)
+        else:
+            self.model_2 = BartForConditionalGeneration.from_pretrained(self.model_name_2)
+        self.model_2.resize_token_embeddings(len(self.tokenizer))
 
         print('Making datasets')
         self.train_dataset = WikiDataset(self.tokenizer, train_x, train_y)
         self.test_dataset = WikiDataset(self.tokenizer, test_x, test_y)
         self.valid_dataset = WikiDataset(self.tokenizer, valid_x, valid_y)
 
-        self.optimizer = AdamW(self.model.get_decoder().parameters(), lr=LEARNING_RATE)
-
+        self.optimizer_1 = AdamW(self.model_1.get_decoder().parameters(), lr=LEARNING_RATE)
+        self.optimizer_2 = AdamW(self.model_2.get_decoder().parameters(), lr=LEARNING_RATE)
         self.trainer = Trainer(
-            model=self.model,
+            model=self.model_1,
             args=trainer_args,
             train_dataset=self.train_dataset,
             eval_dataset=self.valid_dataset,
             tokenizer=self.tokenizer,
-            optimizers=(self.optimizer, None),
+            optimizers=(self.optimizer_1, None),
             # compute_metrics=compute_metrics,
             # callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
         )
+        self.trainer_2 = Trainer(  # new trainer for second model
+            model=self.model_2,
+            args=trainer_args,
+            train_dataset=self.train_dataset,
+            eval_dataset=self.valid_dataset,
+            tokenizer=self.tokenizer,
+            optimizers=(self.optimizer_2, None),
+        )
+
+    def forward(self, inputs):
+        with torch.no_grad():
+            outputs1 = self.model1(inputs)
+            outputs2 = self.model2(inputs)
+        return {'outputs1': outputs1, 'outputs2': outputs2}
 
     def train(self):
         self.trainer.train()
 
     def set_learnable_params(self, freeze_encoder=True, freeze_decoder=True):
 
-        for part in [self.model.get_encoder(), self.model.get_decoder()]:
+        for part in [self.model_1.get_encoder(), self.model_1.get_decoder()]:
             for param in part.embed_positions.parameters():
                 param.requires_grad = False
             for param in part.embed_tokens.parameters():
                 param.requires_grad = False
 
-        for param in self.model.get_encoder().parameters():
+        for param in self.model_1.get_encoder().parameters():
             param.requires_grad = not freeze_encoder
-        for param in self.model.get_decoder().parameters():
+        for param in self.model_1.get_decoder().parameters():
             param.requires_grad = not freeze_decoder
 
     def pred(self):
@@ -74,8 +97,9 @@ class BART:
         inputs, labels, predictions = [], [], []
         with torch.no_grad():
             for batch in tqdm(test_dataloader):
-                ids = self.model.generate(
-                    batch['input_ids'].cuda(), min_length=OUTPUT_GENERATION_MIN_LENGTH, max_length=OUTPUT_GENERATION_MAX_LENGTH
+                ids = self.model_1.generate(
+                    batch['input_ids'].cuda(), min_length=OUTPUT_GENERATION_MIN_LENGTH,
+                    max_length=OUTPUT_GENERATION_MAX_LENGTH
                 )
                 preds = self.tokenizer.batch_decode(ids, skip_special_tokens=True)
                 predictions.extend(preds)
@@ -87,3 +111,6 @@ class BART:
 
     def save(self):
         pass
+
+# in metric lists:
+#  'pearson': PearsonCorrelation('pearson', 'outputs1', 'outputs2')(model_output)
