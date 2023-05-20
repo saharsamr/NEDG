79e38963b4fa5677c3119e0a1ce61a4af6b24723
kghezelbash <kghezelbash12@gmail.com>
2023-05-20 13:21:51 +0330

    completing batch running for assigning the anchor indeces

--- data_handler/assign_anchor_index_to_name_entity.py
+++ data_handler/assign_anchor_index_to_name_entity.py
@@ -1,32 +1,48 @@
-from pymongo import MongoClient
+from pymongo import InsertOne, DeleteMany, ReplaceOne, UpdateOne
 from tqdm import tqdm
 from collections import defaultdict
 
 # Connect to MongoDB
-client = MongoClient('localhost', 27017)
-db = client['my_database']
-collection = db['my_collection']
+client = pymongo.MongoClient('mongodb://localhost:27017/', username='user', password='pass')
+# Creating a database
+db = client['wikipedia']
+
+# Creating a collection
+collection = db['dump']
 
 # Set batch size
-batch_size = 2000
+batch_size = 4000
 
 # Get total number of documents
 total_documents = collection.count_documents({})
 
 # Process documents in batches
+anchor_to_ids = defaultdict(list)
 for i in tqdm(range(0, total_documents, batch_size)):
     # Iterate over all documents in the collection
     documents = collection.find().skip(i).limit(batch_size)
-    updates = []
     for doc in tqdm(documents):
-        entity = doc['entity']
         anchors = doc['anchors']
-
+        id = doc['_id']
         # Iterate over all anchors
-        for anchor, i in anchors:
-            # Find all documents that have this anchor as a name entity
-            matching_docs = collection.find({'title': anchor})
-            for matching_doc in matching_docs:
-                matching_docs['paragraph_anchor_index'] = doc['id']
-                collection.save(matching_docs)
+        for anchor, paragraph_index in anchors:
+            # removing the "" from beginning and the end
+            anchor = anchor[1:-1]
+            # updating the anchor to ids dict
+            anchor_to_ids[anchor].append(id)
+
+# Set batch size
+batch_size = 2000
+for i in tqdm(range(0, total_documents, batch_size)):
+    documents = collection.find().skip(i).limit(batch_size)
+    updates = []
+    for document in tqdm(documents):
+        updates.append(UpdateOne({'_id': document['_id']}, {'$set': {"anchor_to_wikipage_ids": anchor_to_ids[document['title']]}}))
+
+    collection.bulk_write(updates)
+# Find all documents that have this anchor as a name entity
+# matching_docs = collection.find({'title': anchor})
+# for matching_doc in matching_docs:
+#     matching_docs['paragraph_anchor_index'] = doc['id']
+#     collection.save(matching_docs)
 
