f0fbf0c4c24141eba8d17081279575445506b038
kghezelbash <kghezelbash12@gmail.com>
2023-05-20 12:19:14 +0330

    trying to add batch for assigning anchor index but it is not completed

--- data_handler/assign_anchor_index_to_name_entity.py
+++ data_handler/assign_anchor_index_to_name_entity.py
@@ -1,21 +1,32 @@
 from pymongo import MongoClient
+from tqdm import tqdm
+from collections import defaultdict
 
 # Connect to MongoDB
 client = MongoClient('localhost', 27017)
 db = client['my_database']
 collection = db['my_collection']
 
-# Iterate over all documents in the collection
-for doc in collection.find():
-    entity = doc['entity']
-    contexts = doc['contexts']
-    anchors = doc['anchors']
+# Set batch size
+batch_size = 2000
 
-    # Iterate over all anchors
-    for anchor, i in anchors:
-        # Find all documents that have this anchor as a name entity
-        matching_docs = collection.find({'title': anchor})
-        for matching_doc in matching_docs:
-            matching_docs['paragraph_anchor_index'] = doc['id']
-            collection.save(matching_docs)
+# Get total number of documents
+total_documents = collection.count_documents({})
+
+# Process documents in batches
+for i in tqdm(range(0, total_documents, batch_size)):
+    # Iterate over all documents in the collection
+    documents = collection.find().skip(i).limit(batch_size)
+    updates = []
+    for doc in tqdm(documents):
+        entity = doc['entity']
+        anchors = doc['anchors']
+
+        # Iterate over all anchors
+        for anchor, i in anchors:
+            # Find all documents that have this anchor as a name entity
+            matching_docs = collection.find({'title': anchor})
+            for matching_doc in matching_docs:
+                matching_docs['paragraph_anchor_index'] = doc['id']
+                collection.save(matching_docs)
 
