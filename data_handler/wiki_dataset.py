7f41ae9c8a8b72eeabc04bf810dd41d4c7583cb8
saharsamr <sahar.rajabi76@gmail.com>
2023-05-24 15:01:29 +0330

    add parameters from config file

--- data_handler/wiki_dataset.py
+++ data_handler/wiki_dataset.py
@@ -1,10 +1,11 @@
 from torch.utils.data import Dataset
 import torch
+from config import INPUT_GENERATION_MAX_LENGTH, OUTPUT_GENERATION_MAX_LENGTH
 
 
 class WikiDataset(Dataset):
 
-    def __init__(self, tokenizer, inputs, labels, mask_entity=False, max_length=256):
+    def __init__(self, tokenizer, inputs, labels, mask_entity=False, max_length=INPUT_GENERATION_MAX_LENGTH):
 
         self.inputs = inputs
         self.labels = labels
@@ -35,8 +36,10 @@ class WikiDataset(Dataset):
 
     def __getitem__(self, idx):
 
-        input_encodings = self.tokenizer(self.inputs[idx], padding='max_length', truncation=True, max_length=self.max_len)
-        output_encodings = self.tokenizer(self.labels[idx], padding='max_length', truncation=True, max_length=15)
+        input_encodings = self.tokenizer(
+            self.inputs[idx], padding='max_length', truncation=True, max_length=self.max_len)
+        output_encodings = self.tokenizer(
+            self.labels[idx], padding='max_length', truncation=True, max_length=OUTPUT_GENERATION_MAX_LENGTH)
 
         if self.mask_entity:
             input_encodings['input_ids'], input_encodings['attention_mask'] = \
