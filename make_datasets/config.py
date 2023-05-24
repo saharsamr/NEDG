d56e900e8a99e36a915cb75f01e5c106021d6665
saharsamr <sahar.rajabi76@gmail.com>
2023-05-24 12:47:23 +0330

    add source of definitions to jsonl file names

--- make_datasets/config.py
+++ make_datasets/config.py
@@ -13,12 +13,11 @@ MAX_ENTITY_NAME_LENGTH = 5
 MIN_CONTEXT_LENGTH = 20
 WIKI_DUMP_JSONL_PATH = '../data/wikipedia/wiki_dump.jsonl'
 
-TRAIN_JSONL_PATH = '../data/wikipedia/train.jsonl'
-TEST_JSONL_PATH = '../data/wikipedia/test.jsonl'
-VAL_JSONL_PATH = '../data/wikipedia/val.jsonl'
-
 TRAIN_SHARE = 0.8
 TEST_SHARE = 0.1
 VAL_SHARE = 0.1
 
 SOURCE_DEFINITION = 'wikipedia'
+TRAIN_JSONL_PATH = f'../data/wikipedia/train_{SOURCE_DEFINITION}.jsonl'
+TEST_JSONL_PATH = f'../data/wikipedia/test_{SOURCE_DEFINITION}.jsonl'
+VAL_JSONL_PATH = f'../data/wikipedia/val_{SOURCE_DEFINITION}.jsonl'
--- make_datasets/split_data.py
+++ make_datasets/split_data.py
@@ -8,14 +8,16 @@ with open(WIKI_DUMP_JSONL_PATH, 'r') as data_f:
     with open(TRAIN_JSONL_PATH, 'w+') as train_f, (TEST_JSONL_PATH, 'w+') as test_f, (VAL_JSONL_PATH, 'w+') as val_f:
 
         for entity in data_f.readlines():
-            rand = random.random()
             json_obj = json.loads(entity)
+
             if SOURCE_DEFINITION == 'wikipedia':
                 if json_obj['wikipedia_description'] is None or json_obj['wikipedia_description'] == '':
                     continue
             elif SOURCE_DEFINITION == 'wikidata':
                 if json_obj['wikidata_description'] is None or json_obj['wikidata_description'] == '':
                     continue
+
+            rand = random.random()
             if rand < TRAIN_SHARE:
                 train_f.write(entity)
             elif TRAIN_SHARE < rand < TRAIN_SHARE + TEST_SHARE:
