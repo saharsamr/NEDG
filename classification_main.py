bf45f6d3e08918696739de235ed47a5a064543cc
saharsamr <sahar.rajabi76@gmail.com>
2023-05-27 18:31:07 +0330

    add some parameters to trainer

--- classification_main.py
+++ classification_main.py
@@ -41,9 +41,10 @@ def classification_main():
         logging_steps=100,
         evaluation_strategy='steps',
         do_eval=True,
-        eval_steps=100,
-        disable_tqdm=True,
-        load_best_model_at_end=True
+        eval_steps=500,
+        load_best_model_at_end=True,
+        save_strategy='steps',
+        save_total_limit=3
     )
 
     print('Initialing the model...')
