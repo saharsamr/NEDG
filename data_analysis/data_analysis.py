6bedd189e3ac35c5852842a271e84a20c6a2cb60
kghezelbash <kghezelbash12@gmail.com>
2023-05-27 12:50:29 +0330

    defining the data analysis branch and its functions

---
+++ data_analysis/data_analysis.py
@@ -0,0 +1,56 @@
+import json
+
+import matplotlib.pyplot as plt
+
+
+def count_words(sentence):
+    words = sentence.split()
+    return len(words)
+
+
+def distribution_of_entity_context_number(path):
+    with open(path, 'r') as f:
+        number_of_contexts = []
+        for line in f:
+            data = json.loads(line)
+            number_of_contexts.append(len(data['contexts']))
+        plt.hist(number_of_contexts, bins=20)
+        plt.title('Distribution of Number of Contexts per Entity')
+        plt.xlabel('Number of Contexts')
+        plt.ylabel('Frequency')
+        plt.show()
+
+
+#         TODO. we want the number of contexts and their frequencies. right?
+
+
+def distribution_of_contexts_length_in_json(path):
+    # TODO. I suppose that by length you mean number of words
+    with open(path, 'r') as f:
+        context_lengths = []
+        for line in f:
+            data = json.loads(line)
+            for context in data['contexts']:
+                context_lengths.append(count_words(context))
+        plt.hist(context_lengths, bins=20)
+        plt.title('Distribution of Context Lengths in JSONL File')
+        plt.xlabel('Context Length')
+        plt.ylabel('Frequency')
+        plt.show()
+
+
+def files_distribution_of_descriptions_length(path):
+    with open(path, 'r') as f:
+        wikipedia_description_lengths = []
+        wikidata_description_lengths = []
+        for line in f:
+            data = json.loads(line)
+            wikipedia_description_lengths.append(count_words(data['wikipedia_description']))
+            wikidata_description_lengths.append(count_words(data['wikidata_description']))
+        plt.hist(wikipedia_description_lengths, bins=20, alpha=0.5, label='Wikipedia')
+        plt.hist(wikidata_description_lengths, bins=20, alpha=0.5, label='Wikidata')
+        plt.title('Distribution of Description Lengths by Source')
+        plt.xlabel('Description Length')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.show()
