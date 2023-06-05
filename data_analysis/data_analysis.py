1efe58db26a1cd8a8f1f21f21981bdee0652ea6c
kghezelbash <kghezelbash12@gmail.com>
2023-06-05 12:36:49 +0330

    making popularity dict

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -1,6 +1,15 @@
 import json
 
 import matplotlib.pyplot as plt
+from pymongo import InsertOne, DeleteMany, ReplaceOne, UpdateOne
+from tqdm import tqdm
+from collections import defaultdict
+
+from config import MONGODB_LINK, MONGODB_PORT, MONGODB_DATABASE, \
+    MONGODB_COLLECTION, MONGODB_READ_BATCH_SIZE, MONGODB_WRITE_BATCH_SIZE, \
+    MONGODB_PASSWORD, MONGODB_USERNAME
+from pymongo import MongoClient
+import pickle
 
 
 def count_words(sentence):
@@ -21,11 +30,7 @@ def distribution_of_entity_context_number(path):
         plt.show()
 
 
-#         TODO. we want the number of contexts and their frequencies. right?
-
-
 def distribution_of_contexts_length_in_json(path):
-    # TODO. I suppose that by length you mean number of words
     with open(path, 'r') as f:
         context_lengths = []
         for line in f:
@@ -54,3 +59,31 @@ def files_distribution_of_descriptions_length(path):
         plt.ylabel('Frequency')
         plt.legend()
         plt.show()
+
+
+def entity_popularity_check(path):
+    print("Connecting to MongoDB...")
+    client = MongoClient(f'{MONGODB_LINK}:{MONGODB_PORT}/', username=MONGODB_USERNAME, password=MONGODB_PASSWORD)
+    db = client[MONGODB_DATABASE]
+    collection = db[MONGODB_COLLECTION]
+
+    total_documents = collection.count_documents({})
+
+    documents_cursor = collection.find(batch_size=MONGODB_READ_BATCH_SIZE)
+    title_to_popularity = defaultdict(list)
+
+    print("Scanning documents...")
+    for doc in tqdm(documents_cursor, total=total_documents):
+        context_ids = doc['context_ids']
+        title = doc['title']
+        wikipedia_description = doc['wikidata_info']['descriptions']['en']
+        if len(context_ids) == 0 or wikipedia_description == '' or wikipedia_description is None:
+            continue
+        title_to_popularity[title].append(len(context_ids))
+
+    #     save the dictionary in the path
+    print("Saving dictionary to file...")
+    with open(path, 'wb') as file:
+        pickle.dump(title_to_popularity, file)
+
+    print("Dictionary saved successfully.")
