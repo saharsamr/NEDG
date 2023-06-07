acdc1491499602ea1efd66e31cc51667c39bec92
saharsamr <sahar.rajabi76@gmail.com>
2023-06-07 11:21:37 +0330

    compute correlation and plot

--- data_analysis/config.py
+++ data_analysis/config.py
@@ -2,3 +2,4 @@ import os
 dirname = os.path.dirname(__file__)
 
 ENTITY_POPULARITY_PATH = f'{dirname}/../data/entity_popularity.pkl'
+CLASSIFICATION_RESULT_PATH = f'{dirname}/../results/1-context-4epoch-wikidata-con+pred1+pred2-classification-9500/test_result_df.pkl'
--- data_analysis/popularity_analysis.py
+++ data_analysis/popularity_analysis.py
@@ -3,12 +3,15 @@ from pymongo import MongoClient
 from collections import defaultdict
 import pickle
 
+import matplotlib.pyplot as plt
+
 from make_datasets.config import MONGODB_LINK, MONGODB_PORT, MONGODB_DATABASE, \
     MONGODB_COLLECTION, MONGODB_READ_BATCH_SIZE, MONGODB_PASSWORD, MONGODB_USERNAME
-from data_analysis.config import ENTITY_POPULARITY_PATH
+from data_analysis.config import ENTITY_POPULARITY_PATH, CLASSIFICATION_RESULT_PATH
+from data_analysis.utils import compute_metrics, compute_correlation
 
 
-def find_entity_popularity(path):
+def find_entity_popularity():
 
     print("Connecting to MongoDB...")
     client = MongoClient(f'{MONGODB_LINK}:{MONGODB_PORT}/', username=MONGODB_USERNAME, password=MONGODB_PASSWORD)
@@ -27,10 +30,49 @@ def find_entity_popularity(path):
 
     # save the dictionary in the path
     print("Saving dictionary to file...")
-    with open(path, 'wb') as file:
+    with open(ENTITY_POPULARITY_PATH, 'wb') as file:
         pickle.dump(title_to_popularity, file)
 
     print("Dictionary saved successfully.")
 
 
-find_entity_popularity(ENTITY_POPULARITY_PATH)
+def map_metric_to_popularity(title_to_popularity, classification_result):
+
+    for i, row in classification_result.iterrows():
+        title = row['title']
+        popularity = title_to_popularity[title]
+        classification_result.at[i, 'popularity'] = popularity
+
+    return classification_result
+
+
+def plot_correlation(first, second, x_label, y_label):
+
+      plt.scatter(first, second)
+      plt.xlabel(x_label)
+      plt.ylabel(y_label)
+      plt.savefig(y_label)
+
+
+def popularity_and_performance_correlation():
+
+    with open(ENTITY_POPULARITY_PATH, 'rb') as file:
+        title_to_popularity = pickle.load(file)
+    with open(CLASSIFICATION_RESULT_PATH, 'rb') as file:
+        classification_result = pickle.load(file)
+
+    classification_result = map_metric_to_popularity(title_to_popularity, classification_result)
+    classification_result = classification_result[classification_result['label'] != '']
+    metrics = compute_metrics(classification_result)
+
+    print(compute_correlation(metrics['CPE']['bleu'], metrics['CPE']['popularity']))
+    # print(compute_correlation(metrics['CME']['bleu'], metrics['CME']['popularity']))
+    # print(compute_correlation(metrics['Hybrid']['bleu'], metrics['Hybrid']['popularity']))
+
+    plot_correlation(metrics['CPE']['popularity'], metrics['CPE']['bleu'], 'popularity', 'CPE-bertscore')
+    # plot_correlation(metrics['CME']['popularity'], metrics['CME']['bleu'], 'popularity', 'CME-bertscore')
+    # plot_correlation(metrics['Hybrid']['popularity'], metrics['Hybrid']['bleu'], 'popularity', 'Hybrid-bertscore')
+
+
+# find_entity_popularity()
+popularity_and_performance_correlation()
