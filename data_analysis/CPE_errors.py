3b936934983c910153c40fa1f0602e6d03ef29fb
saharsamr <sahar.rajabi76@gmail.com>
2023-06-07 14:16:21 +0330

    fix minor bug

--- data_analysis/CPE_errors.py
+++ data_analysis/CPE_errors.py
@@ -23,7 +23,7 @@ def compare_lowest_bertscores():
         cme_bert = np.mean(classification_result_to_analyze["CME-bert"].values)
 
         print(
-            f'For the {(1/i) * 100} percent of the lowest CPE bertscores, we have:\n'
+            f'For the {i * 100} percent of the lowest CPE bertscores, we have:\n'
             f'mean of bertscores in CPE: {cpe_bert}\n'
             f'and in CME:{cme_bert}\n'
             f'and the t-test results:\n'
@@ -34,9 +34,9 @@ def compare_lowest_bertscores():
         cpe_bertscores.append(cpe_bert)
         cme_bertscores.append(cme_bert)
 
-    plt.plot(np.arange(0.1, 1.1, 0.1), cme_bertscores, 'o-', label='CME')
-    plt.plot(np.arange(0.1, 1.1, 0.1), cpe_bertscores, 'o-', label='CPE')
-    plt.xticks(np.arange(0.1, 1.1, 0.1))
+    plt.plot(np.arange(10, 110, 10), cme_bertscores, 'o-', label='CME')
+    plt.plot(np.arange(10, 110, 10), cpe_bertscores, 'o-', label='CPE')
+    plt.xticks(np.arange(10, 110, 10))
     plt.legend()
     plt.xlabel('Percentage of whole data selected based on lowest Bertscore on CPE')
     plt.ylabel('Bertscore')
