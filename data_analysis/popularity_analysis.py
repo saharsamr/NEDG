c42c28263abc6b3c109cd6b11e91f1dad0a29460
saharsamr <sahar.rajabi76@gmail.com>
2023-06-07 14:40:07 +0330

    add deciles analysis base on entity popularities

--- data_analysis/popularity_analysis.py
+++ data_analysis/popularity_analysis.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 import pickle
 
 import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 from data_analysis.config import ENTITY_POPULARITY_PATH, CLASSIFICATION_RESULT_PATH, JSONL_PATH
 from data_analysis.utils import compute_metrics, compute_correlation
@@ -64,5 +66,47 @@ def popularity_and_performance_correlation():
     # plot_correlation(metrics['Hybrid']['popularity'], metrics['Hybrid']['bleu'], 'popularity', 'Hybrid-bertscore')
 
 
+def compare_highest_popularity():
+
+    with open(ENTITY_POPULARITY_PATH, 'rb') as file:
+        title_to_popularity = pickle.load(file)
+    with open(CLASSIFICATION_RESULT_PATH, 'rb') as file:
+        classification_result = pickle.load(file)
+
+    classification_result = map_metric_to_popularity(title_to_popularity, classification_result)
+
+    cpe_bertscores, cme_bertscores = [], []
+    for i in np.arange(0.1, 1.1, 0.1):
+
+        classification_result.sort_values(by='CPE-bert', inplace=True, ascending=False)
+        classification_result = classification_result[classification_result['CPE-bert'] != 0]
+        classification_result = classification_result[classification_result['CME-bert'] != 0]
+        classification_result_to_analyze = classification_result[int((i-0.1)*len(classification_result)):int(i*len(classification_result))]
+
+        cpe_bert = np.mean(classification_result_to_analyze["CPE-bert"].values)
+        cme_bert = np.mean(classification_result_to_analyze["CME-bert"].values)
+
+        print(
+            f'For the {int(i * 10)}th 10 percent of the most popular entities, we have:\n'
+            f'mean of bertscores in CPE: {cpe_bert}\n'
+            f'and in CME:{cme_bert}\n'
+            f'and the t-test results:\n'
+            f'{stats.ttest_ind(classification_result_to_analyze["CPE-bert"].values, classification_result_to_analyze["CME-bert"].values)}'
+        )
+        print('-------------------------')
+
+        cpe_bertscores.append(cpe_bert)
+        cme_bertscores.append(cme_bert)
+
+    plt.plot(np.arange(10, 110, 10), cme_bertscores, 'o-', label='CME')
+    plt.plot(np.arange(10, 110, 10), cpe_bertscores, 'o-', label='CPE')
+    plt.xticks(['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th'])
+    plt.legend()
+    plt.xlabel('Decile of data')
+    plt.ylabel('Bertscore')
+    plt.savefig('decile-popularity.svg')
+
+
 # find_entity_popularity()
-popularity_and_performance_correlation()
+# popularity_and_performance_correlation()
+compare_highest_popularity()
