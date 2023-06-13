77abb1966de5737025b1df74faef5339ad9fa20f
saharsamr <sahar.rajabi76@gmail.com>
2023-06-13 14:36:38 +0330

    add cardinality analysis

---
+++ data_analysis/ambiguity_analysis.py
@@ -0,0 +1,104 @@
+import json
+from collections import defaultdict
+import re
+from tqdm import tqdm
+from pymongo import MongoClient
+import requests
+import pickle
+
+
+from make_datasets.config import MONGODB_LINK, MONGODB_PORT, MONGODB_DATABASE, \
+    MONGODB_COLLECTION, MONGODB_READ_BATCH_SIZE, MONGODB_PASSWORD, \
+    MONGODB_USERNAME
+from data_analysis.config import ENTITY_NAME_CARDINALITY_PATH, ENTITY_POPULARITY_PATH, \
+    ENTITY_PAGE_VIEW_PATH, ENTITY_WIKIDATA_ID_PATH, TRAIN_JSONL_PATH, TEST_JSONL_PATH, VAL_JSONL_PATH
+
+
+def extract_file_mentions(file, cardinality):
+    for entity in tqdm(file.readlines()):
+        entity = json.loads(entity)
+        for context in entity['contexts']:
+            mentions = re.findall(r'<NE>(.*?)</NE>', context)
+            for mention in mentions:
+                cardinality[mention].add(entity['wikipedia_title'])
+
+    return cardinality
+
+
+def extract_wikidata_ids(cardinality):
+
+    title_set = []
+    for k, v in tqdm(cardinality.items()):
+        title_set.extend(v)
+    title_set = set(title_set)
+
+    client = MongoClient(f'{MONGODB_LINK}:{MONGODB_PORT}/', username=MONGODB_USERNAME, password=MONGODB_PASSWORD)
+    db = client[MONGODB_DATABASE]
+    collection = db[MONGODB_COLLECTION]
+    documents_cursor = collection.find(
+        {'title': {'$in': title_set}}, {'wikidata_info.id': 1, 'id': 1},
+        batch_size=MONGODB_READ_BATCH_SIZE)
+
+    title_to_wikidata_id = {}
+    for doc in documents_cursor:
+        title_to_wikidata_id[doc['title']] = doc['wikidata_info']['id']
+
+    with open(ENTITY_WIKIDATA_ID_PATH, 'w') as f:
+        json.dump(title_to_wikidata_id, f)
+
+    return title_to_wikidata_id
+
+
+def extract_page_view(title_to_wikidata_id):
+
+    title_to_view = {}
+    for title, id_ in title_to_wikidata_id.items():
+
+        get_req = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" \
+                  f"en.wikipedia.org/all-access/user/{id_}/monthly/2023010100/2023020100"
+
+        headers = {"Accept": "application/json"}
+
+        resp = requests.get(get_req, headers=headers)
+        print(resp.content)
+        details = resp.json()
+
+        item_view = 0
+        for item in details['items']:
+            item_view += item['view']
+
+        title_to_view[title] = item_view
+
+    with open(ENTITY_PAGE_VIEW_PATH, 'w') as f:
+        json.dump(title_to_view, f)
+
+    return title_to_view
+
+
+def extract_cardinality_and_popularity(train_path, test_path, valid_path):
+
+    cardinality = defaultdict(set)
+    with open(train_path, 'r') as file:
+        cardinality = extract_file_mentions(file, cardinality)
+
+    with open(test_path, 'r') as file:
+        cardinality = extract_file_mentions(file, cardinality)
+
+    with open(valid_path, 'r') as file:
+        cardinality = extract_file_mentions(file, cardinality)
+
+    cardinality = {k: list(v) for k, v in tqdm(cardinality.items()) if len(v) > 1}
+    # title_to_wikidata_id = extract_wikidata_ids(cardinality)
+
+    with open(ENTITY_POPULARITY_PATH, 'rb') as file:
+        entity_popularity = pickle.load(file)
+
+    cardinality = {k: {t: entity_popularity[t] for t in v} for k, v in tqdm(cardinality.items())}
+
+    with open(ENTITY_NAME_CARDINALITY_PATH, 'w') as file:
+        json.dump(cardinality, file)
+
+
+extract_cardinality_and_popularity(
+    TRAIN_JSONL_PATH, TEST_JSONL_PATH, VAL_JSONL_PATH
+)
--- data_analysis/config.py
+++ data_analysis/config.py
@@ -3,6 +3,14 @@ dirname = os.path.dirname(__file__)
 
 JSONL_PATH = f'{dirname}/../data/wikipedia/wiki_dump.jsonl'
 ENTITY_POPULARITY_PATH = f'{dirname}/../data/entity_popularity.pkl'
+ENTITY_NAME_CARDINALITY_PATH = f'{dirname}/../data/cardinality.json'
+ENTITY_PAGE_VIEW_PATH = f'{dirname}/../data/title_to_view.json'
+ENTITY_WIKIDATA_ID_PATH = f'{dirname}/../data/title_to_wikidata_id.json'
+
+TRAIN_JSONL_PATH = f'{dirname}/../data/wikipedia/train_wikidata.jsonl'
+TEST_JSONL_PATH = f'{dirname}/../data/wikipedia/test_wikidata.jsonl'
+VAL_JSONL_PATH = f'{dirname}/../data/wikipedia/val_wikidata.jsonl'
+
 ENTITY_ALIASES_DICT_PATH = f'{dirname}/../data/entity_aliases.pkl'
 ENTITY_CARDINALITY_PATH = f'{dirname}/../data/cardinality.pkl'
 CLASSIFICATION_RESULT_PATH = f'{dirname}/../results/1-context-4epoch-wikidata-con+pred1+pred2-classification-9500/test_result_df.pkl'
