d94d30c303e84f245f40bf72a6e48a0f3c35692c
saharsamr <sahar.rajabi76@gmail.com>
2023-06-15 16:29:48 +0330

    run ambiguity analysis

--- data_analysis/ambiguity_analysis.py
+++ data_analysis/ambiguity_analysis.py
@@ -135,99 +135,69 @@ def extract_entity_specific_context(file_path, json_dump_path):
         json.dump(cardinality_analysis_data, file)
 
 
-extract_cardinality_and_popularity(
-    TRAIN_JSONL_PATH, TEST_JSONL_PATH, VAL_JSONL_PATH
-)
-
-
 def make_cpe_cme_dataset_for_cardinality_analysis(CPE_model_name, CME_model_name, input_file, output_file,
                                                   delimiter='\1'):
-    # Read the file cardinality data json path
-    with open(ENTITY_NAME_CARDINALITY_PATH, 'r') as file:
-        cardinality = json.load(file)
-
-    # Create input_x and input_y from the contexts and description fields in the cardinality dictionary
-    input_x = []
-    input_y = []
-    for entity_name, entity_dict in cardinality.items():
-        for entity_title, entity_info in entity_dict.items():
-            if 'description' in entity_info:
-                for context in entity_info['contexts']:
-                    input_x.append(context)
-                    input_y.append(entity_info['description'])
-
-    # Train CPE and CME models
     training_args = TrainingArguments(
         output_dir=f'{dirname}/../results',
         logging_dir=LOGGING_DIR,
         logging_strategy='steps',
         logging_steps=100,
     )
-    CPE_model = BART(
-        training_args,
-        input_x, input_y, input_x, input_y, input_x, input_y,
-        load=True, model_load_path=CPE_model_name,
-        model_name=CPE_model_name, mask_entity=False
-    )
-    CME_model = BART(
-        training_args,
-        input_x, input_y, input_x, input_y, input_x, input_y,
-        load=True, model_load_path=CME_model_name,
-        model_name=CME_model_name, mask_entity=True
-    )
 
-    # Create dataframes for the most popular and second most popular entities for each entity name
-    df_most_popular = pd.DataFrame(
-        columns=['context', 'entity_name', 'entity_id', 'masked_description', 'unmasked_description'])
-    df_second_most_popular = pd.DataFrame(
-        columns=['context', 'entity_name', 'entity_id', 'masked_description', 'unmasked_description'])
-    for entity_name, entity_dict in tqdm(cardinality.items()):
-        # Sort the entities by popularity and get the top two
-        entities_sorted = sorted(entity_dict.items(), key=lambda x: x[1]['popularity'], reverse=True)[:2]
-        entity_most_popular, entity_second_most_popular = entities_sorted
+    with open(input_file, 'r') as file:
+        cardinality = json.load(file)
 
-        # Create a dataframe with the context, entity name, entity ID, masked description, and unmasked description
-        # for the most popular entity
-        df_most_popular_entity = pd.DataFrame(
-            columns=['context', 'entity_name', 'entity_id', 'masked_description', 'unmasked_description'])
-        for context in input_x:
-            # Replace the entity name in the context with the most popular entity title
-            context_with_entity = context.replace(entity_name, entity_most_popular[0])
+    most_popular_df = pd.DataFrame(columns=['entity_name', 'entity_title', 'context', 'description'])
+    second_most_popular_df = pd.DataFrame(columns=['entity_name', 'entity_title', 'context', 'description'])
+    for entity_name, entities_dict in cardinality.items():
 
-            # Use CPE and CME to generate the masked and unmasked descriptions
-            masked_desc = CME_model.predict(context_with_entity)
-            unmasked_desc = CPE_model.predict(context_with_entity)
+        entities_dict = {k: v for k, v in sorted(entities_dict.items(), key=lambda item: item[1]['popularity'], reverse=True)}
+        most_popular_entity = list(entities_dict.keys())[0]
+        second_most_popular_entity = list(entities_dict.keys())[1]
+        for context in entities_dict[most_popular_entity]['contexts']:
+            most_popular_df = most_popular_df.append({
+                'entity_name': entity_name,
+                'entity_title': most_popular_entity,
+                'context': context,
+                'description': entities_dict[most_popular_entity]['description']
+            }, ignore_index=True)
+        for context in entities_dict[second_most_popular_entity]['contexts']:
+            second_most_popular_df = second_most_popular_df.append({
+                'entity_name': entity_name,
+                'entity_title': second_most_popular_entity,
+                'context': context,
+                'description': entities_dict[second_most_popular_entity]['description']
+            }, ignore_index=True)
 
-            # Add the context, entity name, entity ID, masked description, and unmasked description to the dataframe
-            df_most_popular_entity = df_most_popular_entity.append({'context': context, 'entity_name': entity_name,
-                                                                    'entity_id': entity_most_popular[0],
-                                                                    'masked_description': masked_desc,
-                                                                    'unmasked_description': unmasked_desc},
-                                                                   ignore_index=True)
+    for df in [most_popular_df, second_most_popular_df]:
+        x = df['context'].values
+        y = df['description'].values
 
-        # Create a dataframe with the context, entity name, entity ID, masked description, and unmasked description
-        # for the second most popular entity
-        df_second_most_popular_entity = pd.DataFrame(
-            columns=['context', 'entity_name', 'entity_id', 'masked_description', 'unmasked_description'])
-        for context in input_x:
-            # Replace the entity name in the context with the second most popular entity title
-            context_with_entity = context.replace(entity_name, entity_second_most_popular[0])
+        CPE_model = BART(
+            training_args,
+            x, y, x, y, x, y,
+            load=True, model_load_path=CPE_model_name,
+            model_name=CPE_model_name, mask_entity=False
+        )
+        CME_model = BART(
+            training_args,
+            x, y, x, y, x, y,
+            load=True, model_load_path=CME_model_name,
+            model_name=CME_model_name, mask_entity=True
+        )
 
-            # Use CPE and CME to generate the masked and unmasked descriptions
-            masked_desc = CME_model.predict(context_with_entity)
-            unmasked_desc = CPE_model.predict(context_with_entity)
+        cpe_pred, cpe_inputs, cpe_labels = CPE_model.pred()
+        cme_pred, cme_inputs, cme_labels = CME_model.pred()
 
-            # Add the context, entity name, entity ID, masked description, and unmasked description to the dataframe
-            df_second_most_popular_entity = df_second_most_popular_entity.append(
-                {'context': context, 'entity_name': entity_name,
-                 'entity_id': entity_second_most_popular[0],
-                 'masked_description': masked_desc,
-                 'unmasked_description': unmasked_desc}, ignore_index=True)
+        df['cpe_pred'] = cpe_pred
+        df['cme_pred'] = cme_pred
 
-        # Add the dataframes to the final dataframes
-        df_most_popular = pd.concat([df_most_popular, df_most_popular_entity], ignore_index=True)
-        df_second_most_popular = pd.concat([df_second_most_popular, df_second_most_popular_entity], ignore_index=True)
+    most_popular_df.to_csv(output_file + '_most_popular.csv', sep=delimiter, index=False)
+    second_most_popular_df.to_csv(output_file + '_second_most_popular.csv', sep=delimiter, index=False)
 
-    # Write the dataframes to file
-    df_most_popular.to_csv(output_file + '_most_popular.csv', sep=delimiter, index=False)
-    df_second_most_popular.to_csv(output_file + '_second_most_popular.csv', sep=delimiter, index=False)
+
+make_cpe_cme_dataset_for_cardinality_analysis(
+    'results/1-context-1epoch-wikidata-CPE',
+    'results/1-context-1epoch-wikidata-CME',
+    CARDINALITY_DATA_JSON_PATH, 'comparison'
+)
