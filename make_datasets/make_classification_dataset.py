c4c0d0c035bbca62852a44b9a824127a320c0d72
saharsamr <sahar.rajabi76@gmail.com>
2023-06-18 11:01:55 +0330

    change applying threshold

--- make_datasets/make_classification_dataset.py
+++ make_datasets/make_classification_dataset.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 def make_classification_dataset(CPE_model_name, CME_model_name, input_file, output_file, split, delimiter='\1'):
 
-    input_data = pd.read_csv(input_file, delimiter=delimiter).sample(frac=0.2, random_state=42).dropna()
+    input_data = pd.read_csv(input_file, delimiter=delimiter).sample(frac=0.05, random_state=42)
     input_x, input_y = list(input_data['contexts']), list(input_data['entity_description'])
 
     training_args = TrainingArguments(
@@ -42,7 +42,15 @@ def make_classification_dataset(CPE_model_name, CME_model_name, input_file, outp
         predictions=CPE_preds, references=CPE_labels, lang='en', model_type='bert-large-uncased')['f1']
     CME_bert = bertscore.compute(
         predictions=CME_preds, references=CME_labels, lang='en', model_type='bert-large-uncased')['f1']
-    classification_labels = [1 if cpe >= cme else 0 for cpe, cme in zip(CPE_bert, CME_bert)]
+
+    CME, CPE = 0, 1
+    classification_labels = [CPE if cpe >= cme else CME for cpe, cme in zip(CPE_bert, CME_bert)]
+
+    classification_labels_t1 = [CPE if cme - cpe <= 0.1 else CME for cpe, cme in zip(CPE_bert, CME_bert)]
+    classification_labels_t2 = [CPE if cme - cpe <= 0.2 else CME for cpe, cme in zip(CPE_bert, CME_bert)]
+    classification_labels_t3 = [CPE if cme - cpe <= 0.3 else CME for cpe, cme in zip(CPE_bert, CME_bert)]
+    classification_labels_t4 = [CPE if cme - cpe <= 0.4 else CME for cpe, cme in zip(CPE_bert, CME_bert)]
+    classification_labels_t5 = [CPE if cme - cpe <= 0.5 else CME for cpe, cme in zip(CPE_bert, CME_bert)]
 
     with open(output_file, 'w') as f, \
       open(f'{DATA_PATH}/{split}_classification_t1.csv', 'w') as f1, \
@@ -68,25 +76,27 @@ def make_classification_dataset(CPE_model_name, CME_model_name, input_file, outp
         writer5 = csv.writer(f5, delimiter=delimiter)
         writer5.writerow(columns)
 
-        for label, title, cpe_context, cpe_pred, cpe_bert, cme_context, cme_pred, cme_bert, class_label in \
+        for label, title, cpe_context, cpe_pred, cpe_bert, cme_context, cme_pred, cme_bert, class_label, \
+            class_label_t1, class_label_t2, class_label_t3, class_label_t4, class_label_t5 in \
             zip(input_y, list(input_data['entity_name']), CPE_inputs, CPE_preds,
-                CPE_bert, CME_inputs, CME_preds, CME_bert, classification_labels):
+                CPE_bert, CME_inputs, CME_preds, CME_bert, classification_labels, classification_labels_t1,
+                classification_labels_t2, classification_labels_t3, classification_labels_t4, classification_labels_t5):
 
             row = [label.replace(delimiter, ''), title.replace(delimiter, ''), cpe_context.replace(delimiter, ''),
                    cpe_pred.replace(delimiter, ''), cpe_bert, cme_context.replace(delimiter, ''),
                    cme_pred.replace(delimiter, ''), cme_bert, class_label]
             writer.writerow(row)
 
-            if abs(cpe_bert - cme_bert) >= 0.1:
-                writer1.writerow(row)
-            if abs(cpe_bert - cme_bert) >= 0.2:
-                writer2.writerow(row)
-            if abs(cpe_bert - cme_bert) >= 0.3:
-                writer3.writerow(row)
-            if abs(cpe_bert - cme_bert) >= 0.4:
-                writer4.writerow(row)
-            if abs(cpe_bert - cme_bert) >= 0.5:
-                writer5.writerow(row)
+            row[-1] = class_label_t1
+            writer1.writerow(row)
+            row[-1] = class_label_t2
+            writer2.writerow(row)
+            row[-1] = class_label_t3
+            writer3.writerow(row)
+            row[-1] = class_label_t4
+            writer4.writerow(row)
+            row[-1] = class_label_t5
+            writer5.writerow(row)
 
 
 make_classification_dataset(CPE_MODEL_NAME, CME_MODEL_NAME, TRAIN_CSV_PATH, TRAIN_CLASSIFICATION_PATH, 'train')
